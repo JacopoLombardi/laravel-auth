f69294bdaffaabacfb30a52f45d3a2c0dc6d4e58
JacopoLombardi <lombajacopo@gmail.com>
2024-05-21 16:51:05 +0200

    [add] added action of LogOut

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -5,9 +5,13 @@
         <div class="container-fluid mx-5">
             <a class="navbar-brand ms-5" href="{{ route('home') }}">Visualizza il Sito</a>
 
-            <div class="d-flex">
-                <a class="nav-link mx-5" href="#">Features</a>
-                <a class="nav-link" href="#">Log Out</a>
+            <div class="d-flex align-items-center">
+                <p class="fw-bold  text-capitalize fs-4 m-0 me-5">{{ Auth::user()->name }}</p>
+
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button type="submit" class="btn btn-danger fw-semibold fs-5 rounded-5 px-3 py-1">Log Out</button>
+                </form>
             </div>
         </div>
       </nav>
