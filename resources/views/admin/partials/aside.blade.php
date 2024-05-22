6eeb598726c8d18d2b4749a7112b19b003dec566
JacopoLombardi <lombajacopo@gmail.com>
2024-05-22 16:39:33 +0200

    [add] Completed Aside bar graphics

---
+++ resources/views/admin/partials/aside.blade.php
@@ -0,0 +1,16 @@
+
+<aside class="bg-dark text-white pt-5">
+    <ul>
+        <li>
+            <a href=""><i class="fa-solid fa-sheet-plastic"></i>Project</a>
+        </li>
+
+        <li>
+            <a href=""><i class="fa-solid fa-compass-drafting"></i>Technology</a>
+        </li>
+
+        <li>
+            <a href=""><i class="fa-solid fa-table-list"></i>Type</a>
+        </li>
+    </ul>
+</aside>
